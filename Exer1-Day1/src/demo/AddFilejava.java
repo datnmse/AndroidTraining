6180b7c3bc54e3166a6a9249911225d7dab200af
datnmse <nguyen.manhdat1309@gmail.com>
2016-01-06 02:18:08 +0700

    Add one more file java

---
+++ Exer1-Day1/src/demo/AddFilejava.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * @author DATNM
+ *
+ */
+public class AddFilejava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a;
+		Integer b;
+		a = "Hi Friend";
+		b=100;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}

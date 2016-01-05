5d5c7c1268bf4d8efc571b5a7e8c5b8fa844f142
datnmse <nguyen.manhdat1309@gmail.com>
2016-01-06 02:15:29 +0700

    demo show git status

--- Exer1-Day1/src/demo/HelloWorld.java
+++ Exer1-Day1/src/demo/HelloWorld.java
@@ -15,6 +15,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		
+		System.out.println("My name is dat");
 	}
 
 }
